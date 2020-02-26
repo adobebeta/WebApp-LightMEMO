f156cffb20f0f4dc63857ca296c67008d330a7b9
Adobe Beta <adobebeta@users.noreply.github.com>
2020-02-26 15:27:05 +0700

    start

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBLE6TWjJWR9W4RKwgYJ82m8GnzrF5yh8E",
+    authDomain: "webapp-lightmemo.firebaseapp.com",
+    databaseURL: "https://webapp-lightmemo.firebaseio.com",
+    projectId: "webapp-lightmemo",
+    storageBucket: "webapp-lightmemo.appspot.com",
+    messagingSenderId: "330924205147",
+    appId: "1:330924205147:web:85e8fac650307682ca0b7e",
+    measurementId: "G-D5FQY3TQ2Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
---
+++ main.js
@@ -0,0 +1,16 @@
+// const form = document.querySelector('#turnOnBtn')
+
+// form.addEventListener("click", function () {
+
+//     document.getElementById('myImage').src = 'pic_bulbon.gif';
+//     var now = new Date().toLocaleTimeString();
+
+//     let firestore = firebase.firestore()
+//     // firestore.collection("Collection").doc("history").set({
+//     firestore.collection("Collection").add({ 
+//         status: "on",
+//         time: now
+//     });
+
+
+// });
